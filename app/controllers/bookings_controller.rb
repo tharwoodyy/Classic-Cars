32a772c409b32df0393b8ab56d44a824d5370664
Iram Shehzadi <Shehzadi193@gmail.com>
2020-02-26 14:28:35 +0000

    changes made

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,32 @@
+class BookingsController < ApplicationController
+	def new
+		@booking = Booking.new
+	end
+
+	def index
+		@bookings = Booking.all
+	end
+
+	def create
+		@booking = Booking.new(booking_params)
+		@car = Car.find(params[:booking][:car_id])
+		@booking.car = @car
+		if @booking.save
+			redirect_to cars_path
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		@booking = Booking.find|(params[:id])
+		@booking.destroy
+		redirect_to car_path(@booking.car)
+	end
+
+	private
+
+		def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :car_id, :user_id)
+		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cars
+  resources :cars do
+      resources :bookings, only: [:create, :new]
+  end
+  resources :bookings, only: [:index]
 end
