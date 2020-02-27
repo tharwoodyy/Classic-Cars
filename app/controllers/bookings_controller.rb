f8acb79c1cf16764432edf103b783ccecfcd2b9d
Iram Shehzadi <Shehzadi193@gmail.com>
2020-02-27 15:51:47 +0000

    review changes and access changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,9 @@
 class BookingsController < ApplicationController
+	before_action :check_booker_access, only: [:edit, :update, :destroy]
 	def new
 		@booking = Booking.new
-    @car = Car.find(params[:car_id])
+		@car = Car.find(params[:car_id])
+		redirect_to car_path(@car) if @car.user == current_user
 	end
 
 	def index
@@ -12,7 +14,8 @@ class BookingsController < ApplicationController
 		@booking = Booking.new(booking_params)
 		@car = Car.find(params[:car_id])
 		@booking.car = @car
-    @booking.user = current_user
+		@booking.user = current_user
+		redirect_to car_path(@car) if @car.user == current_user
 		if @booking.save!
 			redirect_to car_bookings_path
 		else
