c5cb788667f7839243c9c0bc997c674f0d6b993b
Tom Harwood <tharwood26@gmail.com>
2020-02-27 12:54:22 +0000

    added booking index and ability to cancel

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cars do
-      resources :bookings, only: [:create, :new, :index]
+      resources :bookings, only: [:create, :new, :index, :destroy]
   end
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :destroy]
 end
