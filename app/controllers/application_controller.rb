6a1cfabe993e76fb130e2093cc4229ea91c6a52f
Iram Shehzadi <Shehzadi193@gmail.com>
2020-02-28 10:45:58 +0000

    user access complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,20 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 
 	def check_owner_access
-		redirect_to "/" unless current_user.owner?
+		car = Car.find(params[:id])
+		 if current_user.id != car.user.id
+		redirect_to "/"
+		 end
 	end
 		def check_user_access
 		redirect_to "/" unless current_user.user?
 	end
 
-			def check_user_access
+			def check_booker_access
 		redirect_to "/" unless current_user.booker?
+	end
+
+	def owner?(item)
+		item.user.id == current_user.id ? true : false
 	end
 end
